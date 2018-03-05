2e9f3508927c6e001aaa3f001992ecd953e0ee17
Adrian <adrian.draghici96@yahoo.com>
2018-03-05 16:38:19 +0200

    To do list with delegate methods

--- TodoIT/TodoListViewController.swift
+++ TodoIT/TodoListViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     let itemArray = ["Find Mike","Buy Water","Kill Demagorgon","Go to Police","New Commit"]
+    
+    let checkImage = "check-mark"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,12 +26,38 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
-        
+    cell.tintColor = UIColor.red
         return cell
     }
 
-}
+    
+   override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    
+    
+    //MARK-Tableview Delegate Methods
+    
+   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+    print(itemArray[indexPath.row])
+    
+    
+    if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+        tableView.cellForRow(at: indexPath)?.accessoryType = .none
+    } else {
+        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+    }
+    tableView.deselectRow(at: indexPath, animated: true)
+    
+    }
+    
+    }
+
 
