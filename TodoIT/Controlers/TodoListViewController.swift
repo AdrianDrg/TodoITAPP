bfa5ecfcfd181058e2b621f58b534d44c507975b
Adrian <adrian.draghici96@yahoo.com>
2018-03-08 13:58:07 +0200

    Added the category ViewController and implement CoreData.

--- TodoIT/Controlers/TodoListViewController.swift
+++ TodoIT/Controlers/TodoListViewController.swift
@@ -7,92 +7,78 @@
 //
 
 import UIKit
+ import CoreData
+ 
 class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]()
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
-    
+    var selectedCategory : Category? {
+        
+        didSet{
+            
+            loadItems()
+        }
+        
+    }
+   let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-      loadItems()
-   
     }
    
-    
-    //MARK-Tableview Data Source
-    
+    //MARK: - Tableview Data Source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        
         let item = itemArray[indexPath.row]
         
         cell.textLabel?.text = item.title
-        
         cell.accessoryType = item.done ? .checkmark : .none
-        
         cell.tintColor = UIColor.red
         
         return cell
     }
 
-    
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 60
     }
-    
-    
-    
-    //MARK-Tableview Delegate Methods
-    
+   
+    //MARK: - Tableview Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-        
         self.saveItems()
-    
-    tableView.deselectRow(at: indexPath, animated: true)
-    
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
-//MARK-Add New Item
-
+    //MARK: - Add New Item
 @IBAction func AddNewItem(_ sender: UIBarButtonItem) {
         
         var textField = UITextField()
-        
         let alert = UIAlertController(title: "Add a new item", message: "", preferredStyle: .alert)
-        
         let action = UIAlertAction(title: "Add item", style: .default) { (action) in
         
-            let newItem = Item()
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
-            
+            newItem.done = false
+            newItem.parentCategory = self.selectedCategory
             self.itemArray.append(newItem)
             
             self.saveItems()
         }
+    
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
             textField = alertTextField
-            
         }
+    
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
-        
     }
     
-   
-    //MARK-Delete cell on swipe
-    
+    //MARK: - Delete cell on swipe
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
@@ -101,52 +87,76 @@ class TodoListViewController: UITableViewController {
         if (editingStyle == UITableViewCellEditingStyle.delete) {
             // handle delete (by removing the data from your array and updating the tableview)
         
-            
+            //=====DELETE in CRUD=====//
+            context.delete(itemArray[indexPath.row])
+            itemArray.remove(at: indexPath.row)
+    
+            saveItems()
         }
     }
     
+    //=====CREATE in CRUD=====//
     func saveItems() {
-        
-        
-        let encoder = PropertyListEncoder()
-        
         do{
-            let data = try encoder.encode(self.itemArray)
-            try data.write(to: self.dataFilePath!)
-            
+         try context.save()
         }
         catch{
-            
-            print("Error encoding item array, \(error)")
-            
+         print("Error saving context \(error)")
         }
-        
-        
         self.tableView.reloadData()
-        
     }
     
-    
-    
-    func loadItems() {
-     
-       if let data = try? Data(contentsOf: dataFilePath!) {
-        let decoder = PropertyListDecoder()
+    //=====READ in CRUD=====//
+    func loadItems()  {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+       
+        let predicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
         
-            do {
-                itemArray = try decoder.decode([Item].self, from: data)
-
-            } catch {
-              
-                print("Error decoding item array \(error)")
-                
+        request.predicate = predicate
+        
+        do{
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
         }
-        }
-        
-        
+        tableView.reloadData()
     }
-    
-    
 }
+ 
+ 
+ //MARK: - Search bar methods
+ extension TodoListViewController : UISearchBarDelegate {
+    
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key : "title",ascending: true)]
+        
+        do{
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchBar.text?.count == 0 {
+        loadItems()
+            tableView.reloadData()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }else {
+            searchBarSearchButtonClicked(searchBar)
+        }
+        
+    }
+    
+    
+ }
 
 
